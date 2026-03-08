56cdda8037d8246c336cf40d86979b6eeb65159f
spham21 <stephaniepham520@gmail.com>
2026-03-07 16:00:08 -0800

    fix deployment error

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,16 @@ export const metadata = {
   description: "UCLA small business directory",
 };
 
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
